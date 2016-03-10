36b9f2427d8b462a19d9964bf81b531a9df6b3f1
mscho <ms@visitmedia.de>
2016-03-10 18:18:21 +0100

    fixed missing toString conversion for isAlive test

--- src/FindologicClient.php
+++ src/FindologicClient.php
@@ -29,7 +29,7 @@ class FindologicClient
     public function isAlive()
     {
         $response = $this->client->get($this->createRequestUrl(static::ACTION_ISALIVE) . '?shopkey=' . $this->shopKey);
-        return $response->getBody() === 'alive';
+        return (string)$response->getBody() === 'alive';
     }
 
     /**
